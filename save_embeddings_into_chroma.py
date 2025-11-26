93b9fc17bf062a3b5c03af526855ed31ec8913a2
Smriti Bajagain <foreversmriti6@gmail.com>
2025-11-26 20:40:13 +0545

    dict embedding changes for chroma_db

--- save_embeddings_into_chroma.py
+++ save_embeddings_into_chroma.py
@@ -5,35 +5,37 @@ import chromadb
 import pickle
 #loading saved embeddings and texts
 with open("embeddings.pkl", "rb") as f:
-    embeddings = pickle.load(f)
+    saved_data = pickle.load(f)
 
-with open("book_texts.pkl", "rb") as f:
-    book_texts = pickle.load(f)
+embeddings = saved_data["embeddings"]   # list of embedding vectors
+book_texts = saved_data["titles"]       # list of combined book texts
 
 
-client = chromadb.PersistentClient(path="chroma_db") #creating a persistent client that saves the vector database to a folder named chroma_db
+client = chromadb.PersistentClient(path="chroma_db") #creating a persistent client that saves the vector database to a folder named chroma_db (if db doesnt exists)
                                                     #persistentClient ensures your vector database is saved and can be reopened later
 
 collection = client.get_or_create_collection(name="books") #creates collection named books and if already exists it retrieves it
 
 
-#embeddings=(id_, embedding_vector)
-
-ids = []  #book ids
-texts = []  #combined text of title, author, genre, summary
-vectors = []   #embedding vector for each book
-
+#prepare data dictionar
+data = {
+    "ids": [],
+    "documents": [],
+    "embeddings": []
+}
 
 for book_id, emb in embeddings:
-    ids.append(str(book_id))     #chroma requires string ids so converting ids to string
-    texts.append(next(text for i, text in book_texts if i == book_id))   #getting the combined text for the book id and appends the text whose id matches book_id
-    vectors.append(emb)   #appending embedding vector for the book
+    data["ids"].append(str(book_id))  ##chroma requires string ids so converting ids to string
+    text = next(text for i, text in book_texts if i == book_id)  # #getting the combined text for the book id and appends the text whose id matches book_id
+    data["documents"].append(text) #appending combined text for the book
+    data["embeddings"].append(emb) #appending embedding vector for the book
+
 
 #inserting into chroma collection all at once
 collection.add(
-    ids=ids,
-    embeddings=vectors,
-    documents=texts
+    ids=data["ids"],
+    embeddings=data["embeddings"],
+    documents=data["documents"] 
 )
 
 print("stored embeddings in ChromaDB!!!")
