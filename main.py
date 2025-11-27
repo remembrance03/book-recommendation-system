1ec1f010d5d7e2df15bdb2b4b8806bde6f019b37
Smriti Bajagain <foreversmriti6@gmail.com>
2025-11-27 14:25:52 +0545

    testinggg

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from modulefinder import test
 import pickle
 import chromadb
 from chromadb.config import Settings   #used only to configure how ChromaDB should run and how or where it should store/retrieve data from
@@ -16,6 +17,9 @@ book_embeddings = saved_data["embeddings"]  #numpy(numeric) vectors
 
 print("loaded embeddings for", len(book_titles), "books.") 
 
+#testing
+# print(type(book_embeddings[0]))
+# print(book_embeddings[0])
 
 
 
@@ -32,7 +36,7 @@ if collection_name in [c.name for c in client.list_collections()]:  #goes throug
     collection = client.get_collection(collection_name)
 else:
     print("No embeddings collection found. Please run save_embeddings_into_chroma.py first.")
-
+#print(collection.count())
 
 
 ### 
@@ -47,7 +51,8 @@ genai.configure(api_key=api_key) #using api key to authenticate
 ###
 #finally trying to find similar books based on user query
 ###
-def recommend_books(user_query, top_k=5):   #top_k: number of similar books to return
+def recommend_books(user_query, top_k=5, similarity_threshold=25):   #top_k: number of similar books to return 
+                                                                    #similarity_threshold: higher number means less similar; 0.0(perfect match) to 1.0(no similarity at all)
     """
     Recommend books based on user query using:
     SQL exact match and RAG semantic search using Gemini embeddings
@@ -65,9 +70,23 @@ def recommend_books(user_query, top_k=5):   #top_k: number of similar books to r
     results = collection.query(     #ChromaDB method that searches the vector database to find similar embeddings
         query_embeddings=[query_embedding],  #comparing user query embedding with stored embeddings to match similar ones
         n_results=top_k,   #number of similar results to return(top 5)
-        include=["documents"] #includes document texts and their ids of top searches
+        include=["documents", "distances"] #includes document texts and their ids of top searches
     )
 
+    distances = results["distances"][0] #extracting distances of top matches from results
+    best_distance = distances[0]  #smallest distance value (most similar book)
+
+    #testing
+    print("Distance test:")
+    test = collection.query(
+        query_embeddings=[book_embeddings[0][1]],
+        n_results=3,
+        include=["distances", "documents"]
+)
+    print(test)
+
+    if best_distance > similarity_threshold:  #if even the best match is not similar enough
+        return ["Sorry:( we couldn't find any books that match your query in our database. Please try a different query."]
 
     #extracting recommended books from results of top matches
     #chroma db stores multiple query vector all at once so results are stored..
@@ -77,8 +96,9 @@ def recommend_books(user_query, top_k=5):   #top_k: number of similar books to r
                     # }
     #since we only asked one query vector(documents and not its ids) (eg: "documents": [[doc1, doc2..]] ) we take the first element:[0]
     #which results in just [doc1, doc2, doc3,...] for easier access
-    recommended = results["documents"][0]    
-    return recommended  
+    else: 
+        recommended = results["documents"][0]    
+        return recommended  
 
 
 
@@ -87,6 +107,7 @@ def recommend_books(user_query, top_k=5):   #top_k: number of similar books to r
 ###
 while True:
     print("write \"exit\" if u do not want any recommendations")
+    print("\n")
     query = input("what kind of books do you want to read?")
     recommendations = recommend_books(query) #passes the query and fetches recommended books
     if query.lower() == "exit":
